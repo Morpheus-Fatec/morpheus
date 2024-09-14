5ed9a8f4ccb71b4575d6629c24671b75507fe544
Gabriel Alves de Souza <126245773+gabriel15asouza@users.noreply.github.com>
2024-09-14 13:52:28 -0300

    feat: inclusao de ddl e der (#13)
    * ddl
    
    * der
    
    * atualizacao ddl
    
    * alteracao bd

---
+++ back-end/src/main/java/fatec/morpheus/database/ddl.sql
@@ -0,0 +1,46 @@
+create database morpheus;
+use morpheus;
+
+create table Source(
+	src_cod int auto_increment primary key,
+	src_name char(30) unique,
+    src_type enum('1','2'),
+	src_address char(100) unique,
+	src_registry_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP 
+);
+
+create table Tag(
+	tag_cod int auto_increment primary key,
+    tag_name char(20) unique
+);
+
+create table Source_tag(
+	src_tag_cod int auto_increment,
+	src_cod int,
+	tag_cod int,
+    primary key (src_tag_cod),
+	foreign key (src_cod) REFERENCES Source(src_cod),
+	foreign key (tag_cod) REFERENCES Tag(tag_cod)
+);
+
+create table News(
+	new_cod int auto_increment,
+	new_title char(70),
+    new_content text,
+	new_registry_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key (new_cod)
+);
+
+create table Synonymous(
+	tag_cod int,
+	syn_group int,
+    primary key (tag_cod,syn_group),
+	foreign key (tag_cod) REFERENCES Tag(tag_cod)
+);
+
+create table News_tag(
+	new_cod int,
+	src_tag_cod int,
+	foreign key (new_cod) REFERENCES News(new_cod),
+	foreign key (src_tag_cod) REFERENCES Source_tag(src_tag_cod)
+);
