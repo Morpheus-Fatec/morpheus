71e56a85fd6b8746f38083096f4eb113c6c77287
Isaque Costa <isaquesc2@gmail.com>
2024-09-18 20:45:37 -0300

    Realizado as correções conforme a revisão

--- back-end/src/main/java/fatec/morpheus/entity/Tag.java
+++ back-end/src/main/java/fatec/morpheus/entity/Tag.java
@@ -1,19 +1,27 @@
 package fatec.morpheus.entity;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity.BodyBuilder;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter 
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Tag")
-public class CreateTag{
+public class Tag{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +29,9 @@ public class CreateTag{
     private Integer tagCod;
     
     @Column(name = "tag_name", length = 20, unique = true)
-    private String tagName;    
+    private String tagName;
+
+
     
 
 }
