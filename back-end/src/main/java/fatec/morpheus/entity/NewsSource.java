c21ad48f8f68849e08dbb72ec5a6f174e6144386
MateusMSoares <mateus.marques100@hotmail.com>
2024-09-25 23:16:15 -0300

    retirado importacao nao utilizada

--- back-end/src/main/java/fatec/morpheus/entity/NewsSource.java
+++ back-end/src/main/java/fatec/morpheus/entity/NewsSource.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
