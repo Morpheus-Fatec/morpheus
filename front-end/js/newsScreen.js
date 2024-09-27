bc365cf564b9f26b8d1efa80f1019ed12f83d447
Paloma <paloma.rocha01@fatec.sp.gov.br>
2024-09-26 22:10:08 -0300

    delete

--- front-end/js/newsScreen.js
+++ front-end/js/newsScreen.js
@@ -9,115 +9,134 @@ new Vue({
         newsName: '',
         newsAddress: '',
         newsTags: '',
-        newName: '', 
-        newAddress: '', 
-        newTag: [], 
-        listTags:[],
+        newName: '',
+        newAddress: '',
+        newTag: [],
+        listTags: [],
         newTagAdd: {},
-        newPortal:{
-            id:null,
-            tituloModal:"",
-            action:"",
-            erro:{
-                status:false,
-                texto:""
+        newsToDelete: null,
+        pesquisa: {
+            valor: "",
+            campo: "name"
+        },
+        newPortal: {
+            id: null,
+            tituloModal: "",
+            action: "",
+            erro: {
+                status: false,
+                texto: ""
             },
-            tags:[]
+            tags: []
         }
     },
-
-
 
     mounted() {
         this.getNews();
         this.getTags();
     },
 
+    computed: {
+        filteredPortal() {
+            return this.filteredNewsList.filter(portal =>
+                portal[this.pesquisa.campo].toLowerCase().includes(this.pesquisa, valor.toLowerCase())
+            );
+        }
+    },
+
     methods: {
+        openRegister() {
+            this.newPortal.tituloModal = "Cadastrar Portal de Noticias";
+            this.newPortal.action = "Cadastrar";
+            const myModal = new bootstrap.Modal(document.getElementById('modalRegister'));
+            myModal.show();
+        },
 
-    openRegister() {
-        this.newPortal.tituloModal = "Cadastrar Portal de Noticias";
-        this.newPortal.action = "Cadastrar";
-        const myModal = new bootstrap.Modal(document.getElementById('modalRegister'));
-        myModal.show();
-      },
+        editRegister(news) {
+            this.newTag = news.tags;
+            this.newName = news.name;
+            this.newAddress = news.url;
+            this.openRegister();
+            this.newPortal.tituloModal = "Editar Portal de Noticias";
+            this.newPortal.action = "Editar";
+        },
 
-      editRegister(news){
-        this.newTag = news.tags;
-        this.newName = news.name;
-        this.newAddress = news.url;
-        this.openRegister();
-        this.newPortal.tituloModal = "Editar Portal de Noticias";
-        this.newPortal.action = "Editar";
-      },
+        addTag() {
+            newTagAdd = this.listTags.find(item => item.id === this.newsTags);
+            this.newTag.push(newTagAdd);
 
-      addTag(){
-        newTagAdd = this.listTags.find(item => item.id === this.newsTags);
-        this.newTag.push(newTagAdd);
+            const index = this.listTags.findIndex(element => element.id === this.newsTags);
+            if (index !== -1) {
+                this.listTags.splice(index, 1);
+            }
 
-        const index = this.listTags.findIndex(element => element.id === this.newsTags);
-        if (index !== -1) {
-            this.listTags.splice(index, 1);
-        }
+            this.newsTags = {};
+        },
 
-        this.newsTags = {};
-      },
+        removeTag(tag) {
+            const index = this.newTag.findIndex(element => element.id === tag.id);
+            if (index !== -1) {
+                this.newTag.splice(index, 1);
+            }
 
-      removeTag(tag){
-        const index = this.newTag.findIndex(element => element.id === tag.id);
-        if (index !== -1) {
-            this.newTag.splice(index, 1);
-        }
-
-        this.listTags.push(tag);
-      },
-      getTags(){
-        axios
-        .get('https://m3paloma.free.beeceptor.com/tags')
-        .then(response => {
-          this.listTags = response.data.data;
-        })
-        .catch(error => {
-          console.error(error);
-        });
-      },
-      getNews() {
-        axios.get('https://m3paloma.free.beeceptor.com/todos')
-            .then(response => {
-                response.data.forEach(portalNoticia => {
-                    const itemAdd = new Object();
-                    itemAdd.id = portalNoticia.id; 
-                    itemAdd.name = portalNoticia.name; 
-                    itemAdd.url = portalNoticia.url; 
-                    itemAdd.tags = portalNoticia.tags;
-                    this.filteredNewsList.push(itemAdd);
+            this.listTags.push(tag);
+        },
+        getTags() {
+            axios
+                .get('https://apimorpheus1.free.beeceptor.com/tags')
+                .then(response => {
+                    this.listTags = response.data.data;
+                })
+                .catch(error => {
+                    console.error(error);
                 });
+        },
+        getNews() {
+            axios.get('https://m4paloma.free.beeceptor.com/todos')
+            
+                .then(response => {
+                    this.filteredNewsList = [];
+                    response.data.forEach(portalNoticia => {
+                        if (portalNoticia.type == 1) {
+                            const itemAdd = new Object();
+                            itemAdd.id = portalNoticia.id;
+                            itemAdd.name = portalNoticia.srcName;
+                            itemAdd.url = portalNoticia.address;
+                            itemAdd.tags = portalNoticia.tags;
+                            this.filteredNewsList.push(itemAdd);
+                        }
+                    });
 
-            })
-            .catch(error => {
-                console.error("Erro ao carregar dados:", error);
-                alert("Erro ao carregar os dados. Verifique o console para mais detalhes.");
-                throw  error;
+                })
+                .catch(error => {
+                    console.error("Erro ao carregar dados:", error);
+                    alert("Erro ao carregar os dados. Verifique o console para mais detalhes.");
+                    throw error;
 
-            });
-    },    
-
-    postNews(action) {
-        try
-        {
-            this.newPortal.erro.status = false;
-            this.newPortal.erro.texto = '';
-            if (!this.newName.trim() || !this.newAddress.trim()) {
+                });
+        },
+        postNews() {
+            console.log("teste")
+            const nModal = new bootstrap.Modal(document.getElementById('modalRegister'));
+            nModal.hide();
+        },
+        _postNews(action) {
+            try {
+                this.newPortal.erro.status = false;
+                this.newPortal.erro.texto = '';
+                if (!this.newName.trim() || !this.newAddress.trim()) {
                     throw "Preencha todos os campos";
                 };
 
                 const newsSource = {
-                    name: this.newName,
-                    url: this.newUrl,
-                    tags: this.newTag};
-                
-                if(action == "Cadastrar"){
-                    axios.post('https://morpheus-api.free.beeceptor.com/todos', newsSource)
+                    srcName: this.newName,
+                    address: this.newUrl,
+                    type: 1,
+                    tags: this.newTag
+                };
+
+                if (action == "Cadastrar") {
+                    axios.post('https://apimorpheus1.free.beeceptor.com/todos', newsSource)
                         .then(response => {
                             this.getNews();
                             this.resetForm();
@@ -130,7 +149,7 @@ new Vue({
                             this.newPortal.erro.status = true;
                             this.newPortal.erro.texto = "Erro ao cadastrar a notícia. Tente novamente.";
                         });
-                }else{
+                } else {
                     axios.put(`https://morpheus-api.free.beeceptor.com/todos/${this.newPortal.id}`, newsSource)
                         .then(response => {
                             this.getNews();
@@ -144,47 +163,50 @@ new Vue({
                             this.newPortal.erro.status = true;
                             this.newPortal.erro.texto = "Erro ao editar a notícia. Tente novamente.";
                         });
-                                    }
                 }
-        catch (error) {
-            this.newPortal.erro.status = true;
-            this.newPortal.erro.texto = error;
-        }
-    },
+            }
+            catch (error) {
+                this.newPortal.erro.status = true;
+                this.newPortal.erro.texto = error;
+            }
+        },
 
-    openDelete(){
-        const myModal = new bootstrap.Modal(document.getElementById('modalDelete'));
-        myModal.show();
-    },
-
-    save(){
-        this.filteredNewsList.push({
-            id: this.nextId++,             
-            name: this.newName,
-            url: this.newAddress,
-            tag: this.newTag,
-        });
-
-        this.resetForm()
-
-        const myModal = bootstrap.Modal.getInstance(document.getElementById('modalRegister'));
-        myModal.hide();
-    },
+        deleteRegister() {
+            if (this.newsToDelete) {
+                axios.delete(`https://apimorpheus1.free.beeceptor.com/todos/${this.newPortal.id}`)
+                    .then(response => {
+                        this.getNews(); // Atualiza a lista de notícias
+                        this.newsToDelete = null; // Reseta a variável
+                        const myModal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'));
+                        myModal.hide(); // Fecha o modal
+                    })
+                    .catch(error => {
+                        console.error(error);
+                        alert("Erro ao excluir a notícia. Tente novamente."); // Feedback para o usuário
+                    });
+            }
+        },
 
 
-    resetForm() {
-        this.newName = '';
-        this.newAddress = '';
-        this.newTag = '';
+        confirmDelete(news) {
+            this.newsToDelete = news;
+            const myModal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'));
+            myModal.show();
+        },
 
-    },
+        resetForm() {
+            this.newName = '';
+            this.newAddress = '';
+            this.newTag = '';
 
-    filterNews() {
-        const searchLower = this.searchNews.toLowerCase();
-        this.filteredNewsList = this.newsList.filter(news => 
-            news.name.toLowerCase().includes(searchLower) || 
-            news.id.toString().includes(searchLower) // Busca pelo ID
-        );
-      },
+        },
+
+        filterNews() {
+            const searchLower = this.searchNews.toLowerCase();
+            this.filteredNewsList = this.newsList.filter(news =>
+                news.name.toLowerCase().includes(searchLower) ||
+                news.id.toString().includes(searchLower) // Busca pelo ID
+            );
+        },
     },
 })
