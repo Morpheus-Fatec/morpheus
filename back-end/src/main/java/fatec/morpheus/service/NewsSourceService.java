6cafa4043a486414c6b892d499ee847cebbeaf0c
MateusMSoares <74261014+MateusMSoares@users.noreply.github.com>
2024-10-05 14:20:58 -0300

    Tratando excecoes source api (#37)
    * ✨ ajustes na estrutura, camada controller de source protegida
    
    * Adicionado dependencias de validacao do hibernate para regras do tamanho maximo dos campos(InvalidFieldException)
    
    * Correcao do texto de erro
    
    * Correcao do texto de erro

--- back-end/src/main/java/fatec/morpheus/service/NewsSourceService.java
+++ back-end/src/main/java/fatec/morpheus/service/NewsSourceService.java
@@ -1,15 +1,24 @@
 package fatec.morpheus.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import fatec.morpheus.entity.ErrorResponse;
 import fatec.morpheus.entity.NewsSource;
+import fatec.morpheus.exception.InvalidFieldException;
+import fatec.morpheus.exception.NotFoundException;
+import fatec.morpheus.exception.UniqueConstraintViolationException;
 import fatec.morpheus.repository.NewsSourceRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @Service
 public class NewsSourceService {
@@ -17,40 +26,84 @@ public class NewsSourceService {
     @Autowired
     private NewsSourceRepository newsSourceRepository;
 
-    public NewsSource saveNewsSource(NewsSource newsSource) {
-        return newsSourceRepository.save(newsSource);
+    @Autowired
+    private Validator validator;
+
+    public NewsSource createNewsSource(NewsSource newsSource) {
+
+        Set<ConstraintViolation<NewsSource>> violations = validator.validate(newsSource);
+
+        if (!violations.isEmpty()) {
+            List<String> errors = violations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toList());
+
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, errors);
+            throw new InvalidFieldException(errorResponse);
+        }
+
+        try {
+            return newsSourceRepository.save(newsSource);
+                
+        } catch (Exception e) {
+            List<String> duplicateFields = this.verifyUniqueKeys(newsSource);
+            
+            ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.CONFLICT,   
+                duplicateFields       
+            );
+
+            throw new UniqueConstraintViolationException(errorResponse);
+        }
+    }
+
+    private List<String> verifyUniqueKeys(NewsSource newsSource) {
+        List<String> duplicateFields = new ArrayList<>();
+        if (newsSourceRepository.existsBySrcName(newsSource.getSrcName())) {
+            duplicateFields.add("srcName");
+        }
+        if (newsSourceRepository.existsByAddress(newsSource.getAddress())) {
+            duplicateFields.add("address");
+        }
+        return duplicateFields;
     }
 
     public List<NewsSource> findAllNewsSources() {
         return newsSourceRepository.findAll();
     }
 
-    public Optional<NewsSource> findNewsSourceById(int id) {
-        return newsSourceRepository.findById(id);
+    public NewsSource findNewsSourceById(int id) {
+        return newsSourceRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(id));  
     }
 
-    public ResponseEntity<NewsSource> updateNewsSourceById(int id, NewsSource newsSourceToUpdate) {
+    public NewsSource updateNewsSourceById(int id, NewsSource newsSourceToUpdate) {
+        try {
         return newsSourceRepository.findById(id)
                 .map(existingNewsSource -> {
                     existingNewsSource.setSrcName(newsSourceToUpdate.getSrcName());
                     existingNewsSource.setType(newsSourceToUpdate.getType());
                     existingNewsSource.setAddress(newsSourceToUpdate.getAddress());
-
-                    //atualiza tags
                     existingNewsSource.getTags().clear();
                     existingNewsSource.getTags().addAll(newsSourceToUpdate.getTags());
-                    newsSourceRepository.save(existingNewsSource);
-                    return new ResponseEntity<>(existingNewsSource, HttpStatus.OK);
-                })
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
 
-    public ResponseEntity<NewsSource> deleteNewsSourceById(int id) {
-        return newsSourceRepository.findById(id)
-                .map(newsSource -> {
-                    newsSourceRepository.deleteById(id);
-                    return new ResponseEntity<NewsSource>(HttpStatus.OK);
+                    return newsSourceRepository.save(existingNewsSource);
                 })
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException(id));
+        } catch (DataIntegrityViolationException e) {
+            List<String> duplicateFields = this.verifyUniqueKeys(newsSourceToUpdate);
+
+            ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.CONFLICT,  
+                duplicateFields
+            );
+            throw new UniqueConstraintViolationException(errorResponse);
+        }
+    }
+    
+
+    public NewsSource deleteNewsSourceById(int id) {
+        return newsSourceRepository.findById(id)
+                    .orElseThrow(() -> new NotFoundException(id));
     }
 }
