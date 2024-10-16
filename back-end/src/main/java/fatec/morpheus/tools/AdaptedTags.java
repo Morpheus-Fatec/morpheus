6d8348c1cd92c72edfd9428e5b1358aacbecb5cd
Vinicius <vinicius3_1998@hotmail.com>
2024-10-15 22:32:57 -0300

    adicionando lista de sinonimos

--- back-end/src/main/java/fatec/morpheus/tools/AdaptedTags.java
+++ back-end/src/main/java/fatec/morpheus/tools/AdaptedTags.java
@@ -1,13 +1,17 @@
 package fatec.morpheus.tools;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import fatec.morpheus.entity.Synonymous;
 import fatec.morpheus.entity.Tag;
 import fatec.morpheus.entity.Texto;
+import fatec.morpheus.service.SynonymousService;
 import fatec.morpheus.service.TagService;
 import fatec.morpheus.service.TextoService;
 
@@ -15,18 +19,22 @@ import fatec.morpheus.service.TextoService;
 public class AdaptedTags {
     List<String> tagsList = new ArrayList<>();
     List<String> textoList = new ArrayList<>();
+    Map<Integer, Integer> synonymMap = new HashMap<>();
 
     private final TagService tagService;
     private final TextoService textoService;
+    private final SynonymousService synonymousService;
     private final RestTemplate restTemplate;
 
     private final String TAGS_URL = "http://127.0.0.1:8080/morpheus/tag";
     private final String TEXTO_URL = "http://127.0.0.1:8080/textos";
+    private final String SYNONYMOUS_URL = "http://localhost:8080/textos/synonyms";
 
-    public AdaptedTags (TagService tagService, TextoService textoService, RestTemplate restTemplate){
+    public AdaptedTags (TagService tagService, TextoService textoService, SynonymousService synonymousService, RestTemplate restTemplate){
         this.tagService = tagService;
         this.textoService = textoService;
         this.restTemplate = restTemplate;
+        this.synonymousService = synonymousService;
     }
 
     public void allTags(){
@@ -45,5 +53,26 @@ public class AdaptedTags {
                 textoList.add(texto.getTextoDescription());
             }
         }
+    }
+
+    public void allSynonymous() {
+        Synonymous[] synonymousArray = restTemplate.getForObject(SYNONYMOUS_URL, Synonymous[].class);
+
+        if (synonymousArray != null) {
+            for (Synonymous syn : synonymousArray) {
+                synonymMap.put(syn.getTextoCod(), syn.getSynGroup());
+            }
+        }
+    }
+
+    public List<String> chamaFio(){
+        allTags();
+        allTexts();
+        allSynonymous();
+        
+        System.out.println(tagsList);
+        System.out.println(textoList);
+        System.out.println(synonymMap);
+        return tagsList;
     }
 }
