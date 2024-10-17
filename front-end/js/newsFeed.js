f0e3d6418b274fb5a79fd23e8ec508c3dd214e9e
Paloma <paloma.rocha01@fatec.sp.gov.br>
2024-10-16 21:46:12 -0300

    inicialização da tela

---
+++ front-end/js/newsFeed.js
@@ -0,0 +1,109 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            newsList: [],
+            pagination: {
+                totalElements: 0,
+                totalPages: 0,
+                page: 1,
+                itens: 50
+            },
+            dataNews: {
+                title: '',
+                content: '',
+                author: '',
+                registryDate: '',
+                autName: '',
+                srcName: '',
+                srcAddress: ''
+
+            },
+
+            modalContent: {
+                title: '',
+                content: '',
+            },
+
+            sourceNews: {
+                search: {
+                    field: 'srcName',
+                    query: ''
+                }
+            }
+        };
+    },
+
+    methods: {
+        newsLoad() {
+            axios.get(`https://morpheus-api15.free.beeceptor.com/todos?page=${this.pagination.page}&items=${this.pagination.itens}`)
+                .then(response => {
+                    const data = response.data;
+                    this.newsList = [];
+                    data.news.forEach(element => {
+                        const itemAdd = {
+                            newsTitle: element.newsTitle,
+                            newsContent: element.newsContent,
+                            srcName: element.srcName,
+                            scrAddress: element.srcAddress
+                        };
+                        this.newsList.push(itemAdd);
+                    });
+                    this.pagination.totalPages = data.totalPages;
+                    this.pagination.totalElements = data.totalElements;
+
+                    this.newsFilter();
+                })
+                .catch(error => {
+                    console.error('Erro ao carregar as notícias', error);
+                });
+
+        },
+
+        newsFilter() {
+            const query = this.sourceNews.search.query.toLowerCase();
+            const field = this.sourceNews.search.field;
+
+            return this.newsList.filter(news => {
+                if (field === 'name') {
+                    return news.srcName.toLowerCase().includes(query);
+                } else if (field === 'address') {
+                    return news.scrAddress.toLowerCase().includes(query);
+                }
+                return true;
+            });
+        },
+
+        changePage(page) {
+            this.pagination.page = page;
+            this.newsLoad();
+        },
+
+        openModal(content, title) {
+            this.modalContent.content = content;  
+            this.modalContent.title = title;      
+            const modal = new bootstrap.Modal(document.getElementById('content'), {
+                keyboard: false
+            });
+            modal.show();
+        },
+        closeModal() {
+            const modal = bootstrap.Modal.getInstance(document.getElementById('content'));
+            if (modal) {
+                modal.hide(); 
+            }
+        }
+    
+    },
+
+    computed: {
+        filteredNews() {
+            return this.newsFilter(); 
+        }
+    },
+
+    mounted() {
+        this.newsLoad();
+    }
+});
+
+app.mount('#app');
