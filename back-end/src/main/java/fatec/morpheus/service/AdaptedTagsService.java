740a2ac791a0f1049667955106b09801a5451984
Vinicius <vinicius3_1998@hotmail.com>
2024-10-18 21:13:05 -0300

    ajustes solicitados no select

--- back-end/src/main/java/fatec/morpheus/service/AdaptedTagsService.java
+++ back-end/src/main/java/fatec/morpheus/service/AdaptedTagsService.java
@@ -12,21 +12,23 @@ public class AdaptedTagsService {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<String> findVariacao() {
+    public List<String> findVariacao(String tagName) {
         String sql = "SELECT " +
-                    "REPLACE(tg.tag_name, txA.texto_description, txB.texto_description) AS variacao " +
-                    "FROM " +
-                    "Tag tg " +
-                    "JOIN " +
-                    "synonymous tr " +
-                    "ON LOCATE((SELECT texto_description FROM Texto WHERE texto_cod = tr.texto_cod), tg.tag_name) > 0 " +
-                    "JOIN " +
-                    "Texto txA ON txA.texto_cod = tr.texto_cod " +
-                    "JOIN " +
-                    "Texto txB ON txB.texto_cod = tr.syn_group " +
-                    "UNION " +
-                    "SELECT tag_name AS variacao " +
-                    "FROM Tag";
+                "REPLACE(tg.tag_name, txA.texto_description, txB.texto_description) AS variacao " +
+                "FROM " +
+                "Tag tg " +
+                "JOIN " +
+                "synonymous tr " +
+                "ON LOCATE((SELECT texto_description FROM Texto WHERE texto_cod = tr.texto_cod), tg.tag_name) > 0 " +
+                "JOIN " +
+                "Texto txA ON txA.texto_cod = tr.texto_cod " +
+                "JOIN " +
+                "Texto txB ON txB.texto_cod = tr.syn_group " +
+                "WHERE tg.tag_name = '"+ tagName +"'" +
+                "UNION " +
+                "SELECT tag_name AS variacao " +
+                "FROM Tag " +
+                "WHERE tag_name = '" + tagName + "'";
 
         Query query = entityManager.createNativeQuery(sql);
         List<String> resultList = query.getResultList();
