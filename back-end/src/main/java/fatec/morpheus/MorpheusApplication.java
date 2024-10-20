378046de002b4f0c20126dec4fb591b2c43e37a6
MateusMSoares <mateus.marques100@hotmail.com>
2024-10-19 21:57:16 -0300

    iniciando adaptacao da procura pela tag

--- back-end/src/main/java/fatec/morpheus/MorpheusApplication.java
+++ back-end/src/main/java/fatec/morpheus/MorpheusApplication.java
@@ -1,15 +1,25 @@
 package fatec.morpheus;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import fatec.morpheus.service.AdaptedTagsService;
 
 @SpringBootApplication
 @EnableScheduling
 public class MorpheusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MorpheusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MorpheusApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(AdaptedTagsService adaptedTags) {
+        return args -> {
+            System.out.println(adaptedTags.findVariation("safra abacaxi"));
+        };
+    }
 }
