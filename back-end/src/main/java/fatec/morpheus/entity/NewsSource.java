212a987fa24c4008c481044792a46b411c3cdc95
MateusMSoares <mateus.marques100@hotmail.com>
2024-10-20 19:58:47 -0300

    retirado importacao nao utilizada

--- back-end/src/main/java/fatec/morpheus/entity/NewsSource.java
+++ back-end/src/main/java/fatec/morpheus/entity/NewsSource.java
@@ -12,7 +12,6 @@ import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
