c9f37e9e1070a40407f20fbd934242c8628e6f4c
MateusMSoares <mateus.marques100@hotmail.com>
2024-10-20 19:07:23 -0300

    ajustes para merge

--- back-end/src/main/java/fatec/morpheus/service/ScrapingService.java
+++ back-end/src/main/java/fatec/morpheus/service/ScrapingService.java
@@ -51,10 +51,7 @@ public class ScrapingService {
 
                 System.out.println("PORTAL: " + portalUrl);
 
-                List<Tag> tags = source.getSource().getTags();
-                Map<Tag, List<String>> tagsAndVariations = getTagsVariations(tags);
-                // System.out.println("tagsAndVariations: " + tagsAndVariations);
-                
+                List<String> tagsAndVariations =  getTagsVariations(source.getSource().getCode());
 
                 Map<String, String> tagsClass = Map.of(
                     "content", source.getBody(),
@@ -62,7 +59,7 @@ public class ScrapingService {
                     "date", source.getDate(),
                     "author", source.getAuthor(),
                     "urlNoticiaSalva", portalUrl
-                );
+                );               
 
                 extractNews(portalUrl, tagsClass, tagsAndVariations);
 
@@ -74,7 +71,7 @@ public class ScrapingService {
         System.out.println("Processamento de notícias concluído.");
     }
 
-    private void extractNews(String url, Map<String, String> tagsClass, Map<Tag, List<String>> tagsAndVariations) {
+    private void extractNews(String url, Map<String, String> tagsClass, List<String>tagsAndVariations) {
         try {
             Document document = Jsoup.connect(url).get();
             Elements newsCards = document.select("a");
@@ -99,7 +96,7 @@ public class ScrapingService {
         }
     }
 
-    private void scrapeNewsDetails(String newsUrl, Map<String, String> tagsClass, Map<Tag, List<String>> tagsAndVariations) {
+    private void scrapeNewsDetails(String newsUrl, Map<String, String> tagsClass,List<String>tagsAndVariations) {
         try {
             Document newsPage = Jsoup.connect(newsUrl).get();
     
@@ -118,21 +115,7 @@ public class ScrapingService {
             System.out.println("Verificando conteúdo da notícia...");
     
             boolean foundTag = false;
-    
-            for (Map.Entry<Tag, List<String>> entry : tagsAndVariations.entrySet()) {
-                List<String> variations = entry.getValue();
-                for (String variation : variations) {
-                    System.out.println("Procurando por variação: " + variation);
-                    if (contentString.toLowerCase().contains(variation.toLowerCase())) {
-                        System.out.println("Variação encontrada: " + variation);
-                        foundTags.add(entry.getKey());
-                        foundTag = true;
-                        break;
-                    }
-                }
-                if (foundTag) break; // Para assim que uma tag ou variação for encontrada
-            }
-    
+            
             if (!foundTag) {
                 System.out.println("Conteúdo não contém nenhuma das tags.");
                 return;
@@ -149,17 +132,17 @@ public class ScrapingService {
             System.out.println("Erro ao acessar o link: " + newsUrl);
             e.printStackTrace();
         }
-    }
-     
+    }    
 
-    private Map<Tag, List<String>> getTagsVariations(List<Tag> tags) {
-        Map<Tag, List<String>> tagsAndVariations  = new HashMap<>();
-        for (Tag tag : tags) {
-           tagsAndVariations.put(tag, adaptedTagsService.findVariation(tag.getTagName()));
-        }
-        System.out.println("Tags e variações: ");
-        System.out.println(tagsAndVariations);
-        System.out.println("-------------------------------------------------");
-        return tagsAndVariations;
+    private List<String> getTagsVariations(int newsSourceCode) {
+        // Map<Tag, List<String>> tagsAndVariations  = new HashMap<>();
+        List<String> tags = adaptedTagsService.findVariation(newsSourceCode);
+          
+        return tags;
+    // Tags e variações: 
+    // [safra ananas, safra abacaxi]
+    
+    //     Tags e variações:
+    // [safra ananas, safra aipim, safra macaxeira, safra mandioca, safra abacaxi]
     }
 }
