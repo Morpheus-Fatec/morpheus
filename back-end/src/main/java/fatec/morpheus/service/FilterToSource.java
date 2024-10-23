218a8a99c637103e6f4efddaaa091b4a98b51497
Vinicius <vinicius3_1998@hotmail.com>
2024-10-22 23:31:10 -0300

    Filtro por portal

---
+++ back-end/src/main/java/fatec/morpheus/service/FilterToSource.java
@@ -0,0 +1,32 @@
+package fatec.morpheus.service;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+@Service
+public class FilterToSource {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<String> getNoticesBySourceCode(List<Integer> newSourcesCodes){
+        String sources = newSourcesCodes.stream()
+                                     .map(String::valueOf)
+                                     .collect(Collectors.joining(", "));
+        String sql = "SELECT n.new_content " +
+                     "FROM news n " +
+                     "WHERE n.new_src_cod IN (" + sources + ")";
+
+        Query query = entityManager.createNativeQuery(sql);
+
+        List<String> tags = query.getResultList();
+
+        return tags;
+    }
+}
