0245ffc7a55f402951941e446c96ece69e64a2e3
Vinicius <vinicius3_1998@hotmail.com>
2024-10-24 21:57:52 -0300

    query ajustada

--- back-end/src/main/java/fatec/morpheus/database/queryForFilters.sql
+++ back-end/src/main/java/fatec/morpheus/database/queryForFilters.sql
@@ -1,41 +1,12 @@
 SELECT 
-    n.new_content
-FROM 
-    news n 
-LEFT JOIN 
-    news_author a ON a.new_aut_id = n.new_aut_cod 
-WHERE 
-    new_aut_id IN (1)
-LIMIT 3 OFFSET 0;
-
-SELECT 
-    n.new_content 
-FROM 
-    news n 
-WHERE 
-    n.new_src_cod IN (1)
-LIMIT 3 OFFSET 0;
-
-SELECT 
-    new_content 
-FROM 
-    news 
-WHERE 
-    new_content LIKE '%palavra%'
-LIMIT 3 OFFSET 0;
-
-SELECT 
-    n.new_content 
-FROM 
-    news n 
-WHERE 
-    new_title IN ("", "")
-LIMIT 1 OFFSET 0;
-
-SELECT 
-    new_content 
-FROM 
-    news 
-WHERE 
-    new_registry_date BETWEEN '2024-01-10' AND '2024-12-31'
-LIMIT 5 OFFSET 0;
+n.new_content
+FROM news n
+LEFT JOIN news_author a
+ON n.new_aut_cod = a.new_aut_id
+LEFT JOIN source s
+ON n.new_src_cod = s.src_cod 
+WHERE a.new_aut_id IN (1, 2, 5) AND
+s.src_cod IN (1) AND
+n.new_content LIKE '%por%' AND
+n.new_title IN ("Veja imagens de confronto de torcedores do Peñarol com policiais no RJ", "Varela se pronuncia após ser visto em confusão de torcedores do Peñarol") AND
+n.new_registry_date BETWEEN '2024-01-10' AND '2024-12-31';
