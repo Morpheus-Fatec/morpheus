cfd0926cbc680aa1a49baebcba0df48f6d19b617
Vinicius <vinicius3_1998@hotmail.com>
2024-10-24 22:30:08 -0300

    limit e offset incluídos

--- back-end/src/main/java/fatec/morpheus/database/queryForFilters.sql
+++ back-end/src/main/java/fatec/morpheus/database/queryForFilters.sql
@@ -9,4 +9,5 @@ WHERE a.new_aut_id IN (1, 2, 5) AND
 s.src_cod IN (1) AND
 n.new_content LIKE '%por%' AND
 n.new_title IN ("Veja imagens de confronto de torcedores do Peñarol com policiais no RJ", "Varela se pronuncia após ser visto em confusão de torcedores do Peñarol") AND
-n.new_registry_date BETWEEN '2024-01-10' AND '2024-12-31';
+n.new_registry_date BETWEEN '2024-01-10' AND '2024-12-31'
+LIMIT 2 OFFSET 0;
