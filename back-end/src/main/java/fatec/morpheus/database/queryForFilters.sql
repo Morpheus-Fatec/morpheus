7dfaa5622a18709a5ff28e467bb01528a9497606
Vinicius <vinicius3_1998@hotmail.com>
2024-10-30 17:33:32 -0300

    Adicionando regexp no filtro por conteudo

--- back-end/src/main/java/fatec/morpheus/database/queryForFilters.sql
+++ back-end/src/main/java/fatec/morpheus/database/queryForFilters.sql
@@ -7,7 +7,7 @@ LEFT JOIN source s
 ON n.new_src_cod = s.src_cod 
 WHERE a.new_aut_id IN (1, 2, 5) AND
 s.src_cod IN (1) AND
-n.new_content LIKE '%por%' AND
+n.new_content REGEXP "por" AND
 n.new_title REGEXP "Veja imagens de confronto |Varela" AND
 n.new_registry_date BETWEEN '2024-01-10' AND '2024-12-31'
 LIMIT 2 OFFSET 0;
