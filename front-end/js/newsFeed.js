fd0c87a41be100210b2e7244e29ea308b51f7e5f
Paloma <paloma.rocha01@fatec.sp.gov.br>
2024-11-04 00:16:31 -0300

    filtro de data, autor e portal

--- front-end/js/newsFeed.js
+++ front-end/js/newsFeed.js
@@ -8,7 +8,6 @@ const app = Vue.createApp({
                 page: 1,
                 items: 50
             },
-       
             modalContent: {
                 title: '',
                 content: '',
@@ -18,8 +17,7 @@ const app = Vue.createApp({
                 srcName: '',
                 srcAddress: ''
             },
-
-            sourceNews: {
+            news: {
                 search: {
                     field: 'newsTitle',
                     query: '',
@@ -32,12 +30,41 @@ const app = Vue.createApp({
             },
             isLoading: false,
             root: {
-                    alert: {
-                        show: false,
-                        type: 'warning',                         titleError: 'Erro!',
-                        desc: 'Por favor, preencha todos os campos obrigatórios.'
-                    }
+                alert: {
+                    show: false,
+                    type: 'warning',
+                    titleError: 'Erro!',
+                    desc: 'Por favor, preencha todos os campos obrigatórios.'
+                }
             },
+
+            filters: {
+                portal: {
+                    items: [],
+                    selectItems: []
+                },
+
+                authors: {
+                    items: [],
+                    selectItems: []
+                },
+
+                date: {
+                    dateRange: '',
+                    dateWrite: '', 
+                    dateInit:'',
+                    dateFinal:''
+                },
+
+                title: {
+                    items: [],
+                    selectItems: []
+                },
+            },
+
+            sourceFilters: [],
+            authorFilters: [],
+            tagFilters: [],
 
         };
     },
@@ -55,7 +82,7 @@ const app = Vue.createApp({
                             newsContent: element.newsContent,
                             srcName: element.srcName,
                             scrAddress: element.srcAddress,
-                            autName: element.autName,  
+                            autName: element.autName,
                             newsRegistryDate: element.newsRegistryDate,
                             srcURL: element.srcURL
                         };
@@ -63,29 +90,70 @@ const app = Vue.createApp({
                     });
                     this.pagination.totalPages = data.totalPages;
                     this.pagination.totalElements = data.totalElements;
-        
+
                     this.newsFilter();
-                })
+                             })
                 .catch(error => {
-                    this.newsMontedAlert('danger','Alguma indisponibilidade ocorreu no sistema. Tente novamente mais tarde','Erro ao carregar notícias!');
+                    throw error;
+                    this.newsMontedAlert('danger', 'Alguma indisponibilidade ocorreu no sistema. Tente novamente mais tarde', 'Erro ao carregar notícias!');
                 })
                 .finally(() => {
                     this.isLoading = false;
-                    });
-
-
+                });
         },
 
+        srcLoad() {
+            axios.get('http://localhost:8080/morpheus/source')
+                .then(response => {
+                    this.sourceFilters = response.data.map(portalNoticia => ({
+                        code: portalNoticia.code,
+                        name: portalNoticia.srcName
+                    }));
+                    this.initChoices(); 
+                })
+                .catch(error => {
+                    this.rootMontedAlert('danger', 'portal:Alguma indisponibilidade ocorreu no sistema. Tente novamente mais tarde', 'Não foi possível carregar os dados do portal');
+                });
+        },
+
+        authorLoad() {
+            axios.get('https://morpheus-api20.free.beeceptor.com/todos')
+                .then(response => {
+                    this.authorFilters = response.data.map(author => ({
+                        code: author.code,
+                        name: author.name
+                    }));
+                    this.initChoicesAuthors(); 
+                })
+                .catch(error => {
+                    this.rootMontedAlert('danger', 'autor');
+                });
+        },
+  
+
+        tagsLoad() {
+            this.tagFilters = [];
+            axios.get('http://localhost:8080/morpheus/tag')
+                .then(response => {
+                    this.tagFilters = response.data.map(tag => ({
+                        tagCod: tag.tagCod,
+                        tagName: tag.tagName
+                    }));
+                })
+                .catch(error => {
+                    this.rootMontedAlert('danger', 'tag');
+                })
+        },
 
         newsFilter() {
-            const query = this.sourceNews.search.query.toLowerCase();
-            this.sourceNews.filtered = this.newsList
+            const query = this.news.search.query.toLowerCase();
+            this.news.filtered = this.newsList
                 .filter(news =>
-                    news[this.sourceNews.search.field].toLowerCase().includes(query)
+                    news[this.news.search.field].toLowerCase().includes(query)
                 )
                 .sort((a, b) => {
                     const result = a['newsTitle'].toLowerCase().localeCompare(b['newsTitle'].toLowerCase());
-                    return this.sourceNews.search.sort.order === 'asc' ? result : -result;
+                    return this.news.search.sort.order === 'asc' ? result : -result;
                 });
         },
 
@@ -95,8 +163,8 @@ const app = Vue.createApp({
         },
 
         openModal(content, title, author, data) {
-            this.modalContent.content = content;  
-            this.modalContent.title = title;    
+            this.modalContent.content = content;
+            this.modalContent.title = title;
             this.modalContent.author = author;
             this.modalContent.data = data;
             const modal = new bootstrap.Modal(document.getElementById('content'), {
@@ -107,7 +175,7 @@ const app = Vue.createApp({
         closeModal() {
             const modal = bootstrap.Modal.getInstance(document.getElementById('content'));
             if (modal) {
-                modal.hide(); 
+                modal.hide();
             }
         },
 
@@ -117,33 +185,143 @@ const app = Vue.createApp({
                 type: type,
                 titleError: title,
                 desc: message
-            }
+            };
 
             setTimeout(() => {
-                this.root.formData.alert.show = false;
+                this.root.alert.show = false;
             }, 20000);
         },
 
         newsToggleSort(field) {
-            this.sourceNews.search.sort.field = field;
-            this.sourceNews.search.sort.order = this.sourceNews.search.sort.order === 'asc' ? 'desc' : 'asc';
+            this.news.search.sort.field = field;
+            this.news.search.sort.order = this.news.search.sort.order === 'asc' ? 'desc' : 'asc';
             this.newsFilter();
         },
-    
+
+        initChoices() {
+            if (this.sourceFilters.length) {
+                const choices = new Choices('#choices-select', {
+                    removeItemButton: true,
+                    addItems: true,
+                    duplicateItemsAllowed: false,
+                    paste: true,
+                    editItems: true,
+                    allowHTML: true
+                });
+  
+                choices.clearStore();
+                choices.setChoices(this.sourceFilters, 'code', 'name', false);
+  
+                choices.passedElement.element.addEventListener('change', (event) => {
+                    this.filters.portal.selectItems = Array.from(event.target.selectedOptions).map(option => ({
+                        value: option.value,
+                        label: option.textContent
+                    }));
+                });
+            }
+        },
+
+        initChoicesAuthors(){
+            const choicesAuthors = new Choices('#choices-select-authors', {
+                removeItemButton: true,
+                addItems: true,
+                duplicateItemsAllowed: false,
+                paste: true,
+                editItems: true,
+                allowHTML: true,
+            });
+
+           choicesAuthors.clearStore();
+
+            this.authorFilters.forEach(item => {
+                choicesAuthors.setChoices([{ value: item.code, label: item.name }], 'value', 'label', false);
+            });
+
+            choicesAuthors.passedElement.element.addEventListener('change', (event) => {
+                this.filters.authors.selectItems = Array.from(event.target.selectedOptions).map(option => ({
+                        value: option.value,
+                        label: option.textContent
+                    }));       
+            });
+        },
+
+
+        initDatePicker() {
+            flatpickr("#dateRange", {
+                dateFormat: "d/m/Y",
+                locale: {
+                    firstDayOfWeek: 1,
+                    weekdays: {
+                        shorthand: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+                        longhand: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+                    },
+                    months: {
+                        shorthand: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                        longhand: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+                    }
+                },
+                mode: "range",
+                onChange: (selectedDates) => {
+                    if (selectedDates.length === 2) {
+                        this.filters.date.dateRange = `${this.formatDate(selectedDates[0])} até ${this.formatDate(selectedDates[1])}`;
+                        this.filters.date.dateInit = this.formatDate(selectedDates[0]);
+                        this.filters.date.dateFinal = this.formatDate(selectedDates[1]);
+                        this.filters.date.dateWrite = this.filters.date.dateRange; // Atualiza o modelo de entrada
+                        this.filterData(); // Chame filterData após a seleção de datas
+                    } else {
+                        this.filters.date.dateRange = '';
+                        this.filters.date.dateWrite = ''; // Limpa o campo se menos de duas datas forem selecionadas
+                    }
+                }
+            });
+        },
+        formatDate(date) {
+            // Exemplo de formatação de data
+            const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+            return date.toLocaleDateString('pt-BR', options); // Formatação para o padrão brasileiro
+        },
+
+
+        filterData() {
+            const dataFilter = {
+                textSearch: [],
+                sourcesOrigin: this.filters.portal.selectItems.map(item => item.value),
+                titleSearch: [],
+                dateStart: this.filters.date.dateInit,
+                dateEnd: this.filters.date.dateFinal,
+                author: this.filters.authors.selectItems.map(item => item.value),
+            };
+
+            axios.post('https://morpheus-api20.free.beeceptor.com/todos', dataFilter)
+                .then(response => {
+                    const data = response.data;
+                    this.filters.portal.selectItems = data.sourcesOrigin || this.filters.portal.selectItems;
+                    this.filters.authors.selectItems = data.author || this.filters.authors.selectItems;
+                    this.filters.date.dateInit = data.dateStart || this.filters.date.dateInit;
+                    this.filters.date.dateFinal = data.dateEnds || this.filters.date.dateFinal;
+      })
+                .catch(error => {
+                    console.error('Erro:', error);
+                });
+
+        }
     },
-
-
-    
 
     computed: {
         filteredNews() {
-            return this.newsFilter(); 
+            return this.newsFilter();
         }
     },
 
     mounted() {
+    
         this.newsLoad();
-    }
+        this.srcLoad();
+        this.authorLoad();
+        this.initDatePicker();
+
+
+    },
 });
 
 app.mount('#app');
