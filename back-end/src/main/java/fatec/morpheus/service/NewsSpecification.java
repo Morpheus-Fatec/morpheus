609a8bdf2453561866d0392224b36a9739694e9a
MateusMSoares <mateus.marques100@hotmail.com>
2024-11-08 20:13:42 -0300

    Melhorado performace do filtro no SQL

--- back-end/src/main/java/fatec/morpheus/service/NewsSpecification.java
+++ back-end/src/main/java/fatec/morpheus/service/NewsSpecification.java
@@ -10,42 +10,43 @@ public class NewsSpecification {
 
     public static Specification<News> withFilter(NewsSearchRequest request) {
         return (root, query, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.conjunction();
-
+            Predicate predicate = criteriaBuilder.conjunction(); 
+            Predicate titlePredicate = criteriaBuilder.disjunction(); 
+    
             if (request.getTitleSearch() != null && !request.getTitleSearch().isEmpty()) {
-                Predicate titlePredicate = criteriaBuilder.disjunction();
                 for (String title : request.getTitleSearch()) {
-                    titlePredicate = criteriaBuilder.or(titlePredicate, 
+                    titlePredicate = criteriaBuilder.or(titlePredicate,
                         criteriaBuilder.like(criteriaBuilder.lower(root.get("newsTitle")), "%" + title.toLowerCase() + "%"));
                 }
-                predicate = criteriaBuilder.and(predicate, titlePredicate);
             }
-
+    
+            Predicate textPredicate = criteriaBuilder.conjunction();
             if (request.getTextSearch() != null && !request.getTextSearch().isEmpty()) {
-                Predicate contentPredicate = criteriaBuilder.disjunction();
-                for (String content : request.getTextSearch()) {
-                    contentPredicate = criteriaBuilder.or(contentPredicate, 
-                        criteriaBuilder.like(criteriaBuilder.lower(root.get("newsContent")), "%" + content.toLowerCase() + "%"));
+                for (String text : request.getTextSearch()) {
+                    textPredicate = criteriaBuilder.or(textPredicate,
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("newsContent")), "%" + text.toLowerCase() + "%"));
                 }
-                predicate = criteriaBuilder.and(predicate, contentPredicate);
             }
-
+    
             if (request.getAuthor() != null && !request.getAuthor().isEmpty()) {
                 predicate = criteriaBuilder.and(predicate, root.get("newsAuthor").get("autId").in(request.getAuthor()));
             }
-
+    
             if (request.getSourcesOrigin() != null && !request.getSourcesOrigin().isEmpty()) {
                 predicate = criteriaBuilder.and(predicate, root.get("sourceNews").get("code").in(request.getSourcesOrigin()));
             }
-
+    
             if (request.getDateStart() != null && request.getDateEnd() != null) {
-                predicate = criteriaBuilder.and(predicate, 
-                    criteriaBuilder.between(root.get("newsRegistryDate"), request.getDateStart(), request.getDateEnd()));
-            } 
-
+                predicate = criteriaBuilder.and(predicate,
+                    criteriaBuilder.between(root.get("newsRegistryDate"),
+                        request.getDateStart(),
+                        request.getDateEnd()));
+            }
+    
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.or(titlePredicate));
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.or(textPredicate));
             return predicate;
         };
     }
-    
 }
 
