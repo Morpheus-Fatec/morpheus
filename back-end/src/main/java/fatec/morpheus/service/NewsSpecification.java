56e68dfcb675887c8106992fa444d3fd18b73ef0
MateusMSoares <mateus.marques100@hotmail.com>
2024-11-08 22:35:13 -0300

    Atualizando query para todos os casos

--- back-end/src/main/java/fatec/morpheus/service/NewsSpecification.java
+++ back-end/src/main/java/fatec/morpheus/service/NewsSpecification.java
@@ -10,14 +10,16 @@ public class NewsSpecification {
 
     public static Specification<News> withFilter(NewsSearchRequest request) {
         return (root, query, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.conjunction(); 
-            Predicate titlePredicate = criteriaBuilder.disjunction(); 
+            Predicate predicate = criteriaBuilder.conjunction(); // Main AND
     
+            // Title Search
             if (request.getTitleSearch() != null && !request.getTitleSearch().isEmpty()) {
+                Predicate titlePredicate = criteriaBuilder.disjunction(); // OR for multiple titles
                 for (String title : request.getTitleSearch()) {
                     titlePredicate = criteriaBuilder.or(titlePredicate,
                         criteriaBuilder.like(criteriaBuilder.lower(root.get("newsTitle")), "%" + title.toLowerCase() + "%"));
                 }
+                predicate = criteriaBuilder.and(predicate, titlePredicate);
             }
     
             Predicate textPredicate = criteriaBuilder.conjunction();
@@ -28,25 +30,25 @@ public class NewsSpecification {
                 }
             }
     
+            // Author Filter
             if (request.getAuthor() != null && !request.getAuthor().isEmpty()) {
                 predicate = criteriaBuilder.and(predicate, root.get("newsAuthor").get("autId").in(request.getAuthor()));
             }
     
+            // Sources Filter
             if (request.getSourcesOrigin() != null && !request.getSourcesOrigin().isEmpty()) {
                 predicate = criteriaBuilder.and(predicate, root.get("sourceNews").get("code").in(request.getSourcesOrigin()));
             }
     
+            // Date Range Filter
             if (request.getDateStart() != null && request.getDateEnd() != null) {
                 predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.between(root.get("newsRegistryDate"),
-                        request.getDateStart(),
-                        request.getDateEnd()));
+                    criteriaBuilder.between(root.get("newsRegistryDate"), request.getDateStart(), request.getDateEnd()));
             }
     
-            predicate = criteriaBuilder.and(predicate, criteriaBuilder.or(titlePredicate));
-            predicate = criteriaBuilder.and(predicate, criteriaBuilder.or(textPredicate));
             return predicate;
         };
     }
+        
 }
 
