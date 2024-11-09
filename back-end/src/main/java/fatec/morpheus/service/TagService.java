a5b8ad64330d9ef243b31155357d3defa35af120
gabriel15asouza <gabalves.souza@gmail.com>
2024-11-08 22:23:49 -0300

    fix: query with news related to tags' synonymous

--- back-end/src/main/java/fatec/morpheus/service/TagService.java
+++ back-end/src/main/java/fatec/morpheus/service/TagService.java
@@ -70,12 +70,15 @@ public class TagService {
         String inClause = "("+String.join(",", newsSource.stream()
                 .map(source -> "'" + source + "'")
                 .toArray(String[]::new))+")";
-        
-        String sql = "SELECT t.tag_name, t.tag_cod, COUNT(n.new_cod) AS quant "+
-                "FROM tag t "+
-                "INNER JOIN source_tag st ON t.tag_cod = st.tag_cod "+
-                "LEFT JOIN news n ON n.new_content LIKE CONCAT('%', t.tag_name, '%') "+
-                "WHERE st.src_cod in"+inClause+
+
+        String sql = "SELECT t.tag_name, t.tag_cod, COUNT(DISTINCT n.new_cod) AS quant " +
+                "FROM tag t " +
+                "INNER JOIN source_tag st ON t.tag_cod = st.tag_cod " +
+                "LEFT JOIN news n ON n.new_content LIKE CONCAT('%', t.tag_name, '%') " +
+                "LEFT JOIN synonymous s ON s.syn_group = t.tag_cod " +
+                "LEFT JOIN text syn_text ON syn_text.text_cod = s.text_cod " +
+                "LEFT JOIN news n_syn ON n_syn.new_content LIKE CONCAT('%', syn_text.text_cod, '%') " +
+                "WHERE st.src_cod in "+inClause+
                 " GROUP BY t.tag_name, t.tag_cod";
         Query query = entityManager.createNativeQuery(sql);
         return query.getResultList();
