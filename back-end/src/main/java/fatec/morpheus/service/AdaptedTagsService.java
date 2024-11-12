b695065efd65ad2a509a80073d00aeaa839670ed
MateusMSoares <mateus.marques100@hotmail.com>
2024-11-12 16:25:51 -0300

    Criado query que busca regionalismo a partir de textos do filtro

--- back-end/src/main/java/fatec/morpheus/service/AdaptedTagsService.java
+++ back-end/src/main/java/fatec/morpheus/service/AdaptedTagsService.java
@@ -1,5 +1,6 @@
 package fatec.morpheus.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 
 @Service
 public class AdaptedTagsService {
@@ -37,4 +39,41 @@ public class AdaptedTagsService {
     
             return resultList;
     }
+
+    @Transactional    
+    public List<String> findVariationByText(List<String> listOfText) {
+        // Criar tabela temporária
+        String createTableSql = "CREATE TEMPORARY TABLE IF NOT EXISTS temp_strings (valor VARCHAR(255))";
+        entityManager.createNativeQuery(createTableSql).executeUpdate();
+    
+        // Inserir valores na tabela temporária
+        String insertSql = "INSERT INTO temp_strings (valor) VALUES (:valor)";
+        for (String text : listOfText) {
+            Query insertQuery = entityManager.createNativeQuery(insertSql);
+            insertQuery.setParameter("valor", text);
+            insertQuery.executeUpdate();
+        }
+    
+        // Executar consulta para obter as variações
+        String sqlQuery = "SELECT "
+                + "COALESCE(REPLACE(ts.valor, t.text_description, t2.text_description), ts.valor) AS valor_com_variacao "
+                + "FROM temp_strings ts "
+                + "LEFT JOIN text t "
+                + "  ON LOCATE(t.text_description, ts.valor) > 0 "
+                + "LEFT JOIN synonymous s "
+                + "  ON t.text_cod = s.text_cod "
+                + "LEFT JOIN synonymous s1 "
+                + "  ON s1.syn_group = s.syn_group "
+                + "LEFT JOIN text t2 "
+                + "  ON s1.text_cod = t2.text_cod";
+    
+        Query query = entityManager.createNativeQuery(sqlQuery);
+        List<String> resultList = query.getResultList();
+    
+        // Remover tabela temporária
+        String dropTableSql = "DROP TEMPORARY TABLE IF EXISTS temp_strings";
+        entityManager.createNativeQuery(dropTableSql).executeUpdate();
+    
+        return resultList;
+    }    
 }
