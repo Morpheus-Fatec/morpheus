b0ccecaa6ed1d377e3242570f9445cd90762f127
Elbert Jean <elbertjeandossantos@gmail.com>
2024-11-30 20:30:53 -0300

    feat: apiService com Cron

--- back-end/src/main/java/fatec/morpheus/MorpheusApplication.java
+++ back-end/src/main/java/fatec/morpheus/MorpheusApplication.java
@@ -1,26 +1,14 @@
 package fatec.morpheus;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import fatec.morpheus.service.ApiContentService;
-
 @SpringBootApplication
 @EnableScheduling
-public class MorpheusApplication implements CommandLineRunner {
-
-    @Autowired
-    private ApiContentService apiContentService;
+public class MorpheusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MorpheusApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        apiContentService.searchContentApi();
     }
 }
