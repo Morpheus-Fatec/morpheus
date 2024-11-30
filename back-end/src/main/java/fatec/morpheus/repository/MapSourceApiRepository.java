26e468827d35dd1e0a0c81e8f10e098a73748c56
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-11-30 15:26:03 -0300

    Excluindo classes que não serão utilizadas, em principio

--- back-end/src/main/java/fatec/morpheus/repository/MapSourceApiRepository.java
+++
@@ -1,9 +0,0 @@
-package fatec.morpheus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fatec.morpheus.entity.MapSourceApi;
-
-public interface MapSourceApiRepository extends JpaRepository<MapSourceApi, Integer> {
-    
-}
