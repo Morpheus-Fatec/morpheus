351194b617644e67668fe6844670e7ea7fa4b148
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-11-30 14:37:04 -0300

    criação do map source de api

---
+++ back-end/src/main/java/fatec/morpheus/entity/MapSourceAPI.java
@@ -0,0 +1,42 @@
+package fatec.morpheus.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Data_collected_api")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MapSourceAPI {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dat_coll_api_cod")
+    private int apiCollId;
+
+    @OneToOne
+    @JoinColumn(name = "api_cod", nullable = false, unique = true)
+    @JsonBackReference
+    private int apiId;
+
+    @Column(name = "dat_coll_api_registry_date", nullable = false)
+    private String date;
+
+    @Column(name = "dat_coll_api_content", nullable = false)
+    private String apiContent;
+}
