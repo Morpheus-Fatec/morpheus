3cf76d7643907cd07f6b7bbb852d80d2f03b5456
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-11-30 14:57:48 -0300

    - Parametrização da classe PaginatedNewsResponse com tipo genérico. - Ajuste do método getNewsWithDetails no NewsService para usar o tipo genérico. - Ajuste do método getAllNews no NewsController para refletir a parametrização.

--- back-end/src/main/java/fatec/morpheus/controller/NewsController.java
+++ back-end/src/main/java/fatec/morpheus/controller/NewsController.java
@@ -3,17 +3,17 @@ package fatec.morpheus.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import fatec.morpheus.entity.NewsResponse;
 import fatec.morpheus.entity.PaginatedNewsResponse;
 import fatec.morpheus.service.NewsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -32,8 +32,8 @@ public class NewsController {
     })
 
     @GetMapping
-    public ResponseEntity<PaginatedNewsResponse> getAllNews(@RequestParam (defaultValue = "1")int page, @RequestParam(defaultValue = "50") int itens) {
-        PaginatedNewsResponse news = newsService.getNewsWithDetails(page, itens);
+    public ResponseEntity<PaginatedNewsResponse<NewsResponse>> getAllNews(@RequestParam (defaultValue = "1")int page, @RequestParam(defaultValue = "50") int itens) {
+        PaginatedNewsResponse<NewsResponse> news = newsService.getNewsWithDetails(page, itens);
         return ResponseEntity.ok(news);
     }
 
