e3d81d9914b9e590e6e1e780a393e8592473e777
JulioAraujo92 <julio92.araujo@gmail.com>
2024-11-30 16:54:50 -0300

    Corrigido exclusão em cascata de Source

--- back-end/src/main/java/fatec/morpheus/config/DatabaseConfig.java
+++ back-end/src/main/java/fatec/morpheus/config/DatabaseConfig.java
@@ -22,18 +22,34 @@ public class DatabaseConfig {
     public void init() {
         transactionTemplate.execute(status -> {
 
-            List<String> foreignKeys = entityManager.createNativeQuery(
+
+            List<String> foreignKeysSourceTag = entityManager.createNativeQuery(
                 "SELECT CONSTRAINT_NAME " +
                 "FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE " +
                 "WHERE TABLE_NAME = 'source_tag' AND COLUMN_NAME = 'tag_cod' AND CONSTRAINT_SCHEMA = DATABASE()"
             ).getResultList();
 
-            for (String foreignKey : foreignKeys) {
+            for (String foreignKey : foreignKeysSourceTag) {
                 entityManager.createNativeQuery("ALTER TABLE source_tag DROP FOREIGN KEY " + foreignKey).executeUpdate();
             }
 
             entityManager.createNativeQuery(
                 "ALTER TABLE source_tag ADD CONSTRAINT FK_tag_cod_source_tag FOREIGN KEY (tag_cod) REFERENCES tag(tag_cod) ON DELETE CASCADE"
+            ).executeUpdate();
+
+
+            List<String> foreignKeysNews = entityManager.createNativeQuery(
+                "SELECT CONSTRAINT_NAME " +
+                "FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE " +
+                "WHERE TABLE_NAME = 'news' AND COLUMN_NAME = 'new_src_cod' AND CONSTRAINT_SCHEMA = DATABASE()"
+            ).getResultList();
+
+            for (String foreignKey : foreignKeysNews) {
+                entityManager.createNativeQuery("ALTER TABLE news DROP FOREIGN KEY " + foreignKey).executeUpdate();
+            }
+
+            entityManager.createNativeQuery(
+                "ALTER TABLE news ADD CONSTRAINT FK_new_src_cod_news FOREIGN KEY (new_src_cod) REFERENCES source(src_cod) ON DELETE CASCADE"
             ).executeUpdate();
 
             return null;
